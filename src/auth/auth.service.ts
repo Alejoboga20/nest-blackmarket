c93724a9bda9d465d821a93f0e15a3c6d5e3ccf8
Alejo Boga <alejoboga20@gmail.com>
2023-09-14 16:13:19 -0300

    feat: implement login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
-import { CreateUserDto } from './dto';
+import { CreateUserDto, LoginUserDto } from './dto';
 import { UserRepository } from './repositories/user.repository';
 
 @Injectable()
@@ -9,6 +10,25 @@ export class AuthService {
 
   async register(createUserDto: CreateUserDto) {
     const user = await this.userRepository.createUser(createUserDto);
+
+    return user;
+  }
+
+  async login(loginDto: LoginUserDto) {
+    const { email, password } = loginDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Invalid credentials');
+
+    delete user.password;
 
     return user;
   }
