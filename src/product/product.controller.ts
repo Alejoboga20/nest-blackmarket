4e91c982f194c1a6191f104e3d2e2c48c2e10fb0
Alejo Boga <alejoboga20@gmail.com>
2023-09-15 12:03:34 -0300

    feat: protect endpoint with auth guard

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -8,11 +8,13 @@ import {
   Patch,
   ParseUUIDPipe,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 
 import { CreateProductDto, UpdateProductDto } from '@product/dto';
 import { ProductService } from '@product/product.service';
 import { PaginationDto } from '@common/dto/pagination.dto';
+import { JwtAuthGuard } from '@src/auth/guards/jwt-auth.guard';
 
 @Controller('product')
 export class ProductController {
@@ -28,11 +30,13 @@ export class ProductController {
     return this.productService.findAll(paginationDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(
     @Param('id', ParseUUIDPipe) id: string,
@@ -41,6 +45,7 @@ export class ProductController {
     return this.productService.update(id, updateProductDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   delete(@Param('id', ParseUUIDPipe) id: string) {
     return this.productService.remove(id);
