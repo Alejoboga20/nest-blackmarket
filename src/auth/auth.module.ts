77a311e3dbdb0475eb67cd18a751f2b82f1e4ac6
Alejo Boga <alejoboga20@gmail.com>
2023-09-19 16:28:18 -0300

    feat: use constants in config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,18 +8,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
+const JWT_SECRET = 'JWT_SECRET';
+const PASSPORT_STRATEGY = 'jwt';
+const JWT_EXPIRATION_TIME = '2h';
+
 @Module({
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, ConfigService],
   imports: [
     UserModule,
-    PassportModule.register({ defaultStrategy: 'jwt' }),
+    PassportModule.register({ defaultStrategy: PASSPORT_STRATEGY }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '2h' },
+        secret: configService.get<string>(JWT_SECRET),
+        signOptions: { expiresIn: JWT_EXPIRATION_TIME },
       }),
     }),
   ],
