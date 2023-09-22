79b6466f61ac849b0f9166d137f2646f2da9fea8
Alejo Boga <alejoboga20@gmail.com>
2023-09-22 10:10:54 -0300

    docs: fix auth

--- src/auth/decorators/auth-description.decorator.ts
+++ src/auth/decorators/auth-description.decorator.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/swagger';
 import { LoginUserDto } from '../dto/login-user.dto';
 import { User } from '@src/user/entities/user.entity';
+import { CreateUserDto } from '@src/user/dto';
 
 class AuthResponse {
   @ApiProperty()
@@ -16,15 +17,18 @@ class AuthResponse {
   token: string;
 }
 
-export const AuthDescription = (methodName: string) => {
+type AuthMethods = 'register' | 'login';
+
+export const AuthDescription = (methodName: AuthMethods) => {
   const label = methodName.charAt(0).toUpperCase() + methodName.slice(1);
+  const body = methodName === 'register' ? CreateUserDto : LoginUserDto;
 
   return applyDecorators(
     ApiOperation({
       summary: label,
       description: `Endpoint to ${methodName} a user`,
     }),
-    ApiBody({ type: LoginUserDto }),
+    ApiBody({ type: body }),
     ApiOkResponse({
       description: `The user has been ${methodName} successfully`,
       type: AuthResponse,
