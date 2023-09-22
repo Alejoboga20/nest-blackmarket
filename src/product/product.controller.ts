68c2912d08645977a396d2845cc8d6e939701431
Alejo Boga <alejoboga20@gmail.com>
2023-09-22 10:31:49 -0300

    docs: decorate product controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -16,28 +16,33 @@ import { CreateProductDto, UpdateProductDto } from '@product/dto';
 import { ProductService } from '@product/product.service';
 import { PaginationDto } from '@common/dto/pagination.dto';
 import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
+import { ProductDescription } from './decorators/product-description.decorator';
 
 @ApiTags('Product')
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @ProductDescription('findOne')
   @Get(':searchTerm')
   findOne(@Param('searchTerm') searchTerm: string) {
     return this.productService.findOne(searchTerm);
   }
 
+  @ProductDescription('findAll')
   @Get()
   findAll(@Query() paginationDto: PaginationDto) {
     return this.productService.findAll(paginationDto);
   }
 
+  @ProductDescription('create', { isPrivate: true })
   @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
+  @ProductDescription('update', { isPrivate: true })
   @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(
@@ -47,6 +52,7 @@ export class ProductController {
     return this.productService.update(id, updateProductDto);
   }
 
+  @ProductDescription('delete')
   @UseGuards(JwtAuthGuard)
   @Delete(':id')
   delete(@Param('id', ParseUUIDPipe) id: string) {
